cf5dfdc80641f6638e5632d8c445aa5f8c187ac5
halit <oskan96@gmail.com>
2020-12-26 22:17:50 +0000

    action/reducer/store created

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,8 +1,41 @@
 import React from "react";
 
 class CoursesPage extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      course: {
+        title: "",
+      },
+    };
+    //this.handleChange = this.handleChange.bind(this);
+  }
+  // Class Field...avoiding binding issues in class components
+  handleChange = (event) => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+    console.log(course.title);
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    alert(this.state.course.title);
+  };
+
   render() {
-    return <h2>Courses</h2>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <h2>Course</h2>
+        <h3>Add Course</h3>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
+      </form>
+    );
   }
 }
 
