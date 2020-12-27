c8b02a5582cb01043aa9f5c400b671bdbbab46e8
halit <oskan96@gmail.com>
2020-12-27 22:29:17 +0000

    redux flow working

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,42 +1,54 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from "prop-types";
 
 class CoursesPage extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    course: {
+      title: "",
+    },
+  };
 
-    this.state = {
-      course: {
-        title: "",
-      },
-    };
-    //this.handleChange = this.handleChange.bind(this);
-  }
-  // Class Field...avoiding binding issues in class components
   handleChange = (event) => {
     const course = { ...this.state.course, title: event.target.value };
     this.setState({ course });
-    console.log(course.title);
   };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    alert(this.state.course.title);
+    this.props.dispatch(courseActions.createCourse(this.state.course));
   };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-        <h2>Course</h2>
+        <h2>Courses</h2>
         <h3>Add Course</h3>
         <input
           type="text"
           onChange={this.handleChange}
           value={this.state.course.title}
         />
+
         <input type="submit" value="Save" />
+        {this.props.courses.map((course) => {
+          return <div key={course.title}>{course.title}</div>;
+        })}
       </form>
     );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired,
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
